b14c5f22e8162d7d4001314de92f50dc2365d0a6
Clemens Siebler <clemens.siebler@gmail.com>
2019-03-11 11:15:16 +0100

    Added Blob Example

---
+++ BlobExample/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+import azure.functions as func
+
+def main(inputBlob: func.InputStream, outputBlob: func.Out[str]):
+    logging.info(f"Blob trigger executed!")
+    logging.info(f"Blob Name: {inputBlob.name} ({inputBlob.length}) bytes")
+    logging.info(f"Full Blob URI: {inputBlob.uri}")
+
+    output = "Hello World!"
+    outputBlob.set(output)
